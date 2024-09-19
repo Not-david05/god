7da548f3068cb0026cb248401400c8372af02d13
David Valentin Medina <david_valentin@ceroca.cat>
2024-09-19 19:31:15 +0200

    Borde

--- Assets/Scripts/Nau Jugador.cs
+++ Assets/Scripts/Nau Jugador.cs
@@ -6,10 +6,19 @@ public class NauJugador : MonoBehaviour
 {
     private float _vel;
 
+  private Vector2 minPantalla;
+  private Vector2 maxPantalla;
     // Start is called before the first frame update
     void Start()
     {
-        _vel = 10;
+        _vel = 100;
+        minPantalla= Camera.main.ViewportToWorldPoint(new Vector2(0,0));
+        maxPantalla= Camera.main.ViewportToWorldPoint(new Vector2(1,1));
+
+        minPantalla.x=minPantalla.x + 0.75f;
+        maxPantalla.x=maxPantalla.x - 0.75f;
+        minPantalla.y=minPantalla.y + 0.75f;
+        maxPantalla.y=maxPantalla.y - 0.75f;
     }
 
     // Update is called once per frame
@@ -22,6 +31,9 @@ public class NauJugador : MonoBehaviour
        Vector2 direccionIndicada = new Vector2(direccioIndicadaX,direccioIndicadaY).normalized;
        Vector2 nuevaPos= transform.position;
        nuevaPos = nuevaPos + direccionIndicada * _vel * Time.deltaTime;
+
+       nuevaPos.x=Mathf.Clamp(nuevaPos.x, minPantalla.x,maxPantalla.x );
+       nuevaPos.y=Mathf.Clamp(nuevaPos.y, minPantalla.y,maxPantalla.y );
        transform.position=nuevaPos;
     }
 }
